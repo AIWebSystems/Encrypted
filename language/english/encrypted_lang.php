dbf53964668a8189d89d1bf189192157edb8ccf4
Ryan Thompson <service@aiwebsystems.com>
2013-11-15 09:28:59 -0600

    Initial commit

---
+++ language/english/encrypted_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+// Labels
+$lang['streams:encrypted.name'] 					= 'Encrypted';
+
+// Entry
+$lang['streams:encrypted.hide_typeing']				= 'Hide Typing';
+$lang['streams:encrypted.mode']						= 'Mode';
---
+++ src/Pyro/FieldType/Encrypted.php
@@ -0,0 +1,96 @@
+<?php namespace Pyro\FieldType;
+
+use Pyro\Module\Streams_core\Core\Field\AbstractField;
+
+/**
+ * Streams Encrypted Field Type
+ *
+ * @package		PyroCMS\Addons\Shared Addons\Field Types
+ */
+class Encrypted extends AbstractField
+{
+	public $field_type_slug = 'encrypted';
+
+	public $db_col_type = 'text';
+
+	public $version = '1.0';
+
+	/**
+	 * Custom options
+	 * @var array
+	 */
+	public $custom_parameters = array(
+		'hide_typing',
+		'mode',
+		);
+	
+	/**
+	 * About meh
+	 * @var array
+	 */
+	public $author = array(
+		'name' => 'AI Web Systems, Inc. - Ryan Thompson',
+		'url' => 'http://www.aiwebsystems.com/'
+		);
+
+	///////////////////////////////////////////////////////////////////////////////
+	// --------------------------	METHODS 	  ------------------------------ //
+	///////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Construct
+	 */
+	public function __construct()
+	{
+		ci()->load->library('encrypt');
+	}
+
+	/**
+	 * Output form input
+	 *
+	 * @access 	public
+	 * @return	string
+	 */
+	public function formInput()
+	{
+		return form_input($this->form_slug, $this->value);
+	}
+
+	/**
+	 * Output filter input
+	 *
+	 * @access 	public
+	 * @return	string
+	 */
+	public function filterInput()
+	{
+		return form_input($this->getFilterSlug('contains'), ci()->input->get($this->getFilterSlug('contains')));
+	}
+
+	/**
+	 * Pre Ouput
+	 *
+	 * Process before outputting on the CP. Since
+	 * there is less need for performance on the back end,
+	 * this is accomplished via just grabbing the title column
+	 * and the id and displaying a link (ie, no joins here).
+	 *
+	 * @return	mixed 	null or string
+	 */
+	public function stringOutput()
+	{
+		return $this->value;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////
+	// -------------------------	PARAMETERS 	  ------------------------------ //
+	///////////////////////////////////////////////////////////////////////////////
+
+	///////////////////////////////////////////////////////////////////////////////
+	// -----------------------------	AJAX 	  ------------------------------ //
+	///////////////////////////////////////////////////////////////////////////////
+
+	///////////////////////////////////////////////////////////////////////////////
+	// -------------------------	UTILITIES 	  ------------------------------ //
+	///////////////////////////////////////////////////////////////////////////////
+}
