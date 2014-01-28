54d60de73a1ac22b14cee9b728bcdc6fde870f0a
Ryan Thompson <service@aiwebsystems.com>
2014-01-27 18:09:58 -0600

    Changes

--- src/Pyro/FieldType/Encrypted.php
+++ src/Pyro/FieldType/Encrypted.php
@@ -5,117 +5,117 @@ use Pyro\Module\Streams_core\AbstractFieldType;
 /**
  * Streams Encrypted Field Type
  *
- * @package		PyroCMS\Addons\Shared Addons\Field Types
+ * @package        PyroCMS\Addons\Shared Addons\Field Types
  */
 class Encrypted extends AbstractFieldType
 {
-	public $field_type_slug = 'encrypted';
+    public $field_type_slug = 'encrypted';
 
-	public $db_col_type = 'text';
+    public $db_col_type = 'text';
 
-	public $version = '1.0';
+    public $version = '1.0';
 
-	/**
-	 * Custom options
-	 * @var array
-	 */
-	public $custom_parameters = array(
-		'hide_typing',
-		);
-	
-	/**
-	 * About meh
-	 * @var array
-	 */
-	public $author = array(
-		'name' => 'AI Web Systems, Inc. - Ryan Thompson',
-		'url' => 'http://www.aiwebsystems.com/'
-		);
+    /**
+     * Custom options
+     * @var array
+     */
+    public $custom_parameters = array(
+        'hide_typing',
+        );
+    
+    /**
+     * About meh
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc. - Ryan Thompson',
+        'url' => 'http://www.aiwebsystems.com/'
+        );
 
-	///////////////////////////////////////////////////////////////////////////////
-	// --------------------------	METHODS 	  ------------------------------ //
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    // --------------------------    METHODS       ------------------------------ //
+    ///////////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Construct
-	 */
-	public function __construct()
-	{
-		ci()->load->library('encrypt');
-	}
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        ci()->load->library('encrypt');
+    }
 
-	/**
-	 * Output form input
-	 *
-	 * @access 	public
-	 * @return	string
-	 */
-	public function formInput()
-	{
-		return form_input($this->form_slug, $this->value, 'class="form-control" placeholder="'.$this->getParameter('placeholder', '****************').'"');
-	}
+    /**
+     * Output form input
+     *
+     * @access     public
+     * @return    string
+     */
+    public function formInput()
+    {
+        return form_input($this->form_slug, $this->value, 'class="form-control" placeholder="'.$this->getParameter('placeholder', '****************').'"');
+    }
 
-	/**
-	 * Output filter input
-	 *
-	 * @access 	public
-	 * @return	string
-	 */
-	public function filterInput()
-	{
-		return 'No filter available';
-	}
+    /**
+     * Output filter input
+     *
+     * @access     public
+     * @return    string
+     */
+    public function filterInput()
+    {
+        return 'No filter available';
+    }
 
-	/**
-	 * Process before saving
-	 * @return string
-	 */
-	public function preSave()
-	{
-		// Encrypt the value if any and not our placeholder
-		if ($this->value) {
-			return ci()->encrypt->encode($this->value);
-		}
+    /**
+     * Process before saving
+     * @return string
+     */
+    public function preSave()
+    {
+        // Encrypt the value if any and not our placeholder
+        if ($this->value) {
+            return ci()->encrypt->encode($this->value);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Pre Ouput
-	 *
-	 * Process before outputting on the CP. Since
-	 * there is less need for performance on the back end,
-	 * this is accomplished via just grabbing the title column
-	 * and the id and displaying a link (ie, no joins here).
-	 *
-	 * @return	mixed 	null or string
-	 */
-	public function stringOutput()
-	{
-		return '********';
-	}
+    /**
+     * Pre Ouput
+     *
+     * Process before outputting on the CP. Since
+     * there is less need for performance on the back end,
+     * this is accomplished via just grabbing the title column
+     * and the id and displaying a link (ie, no joins here).
+     *
+     * @return    mixed     null or string
+     */
+    public function stringOutput()
+    {
+        return '********';
+    }
 
-	///////////////////////////////////////////////////////////////////////////////
-	// -------------------------	PARAMETERS 	  ------------------------------ //
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    // -------------------------    PARAMETERS       ------------------------------ //
+    ///////////////////////////////////////////////////////////////////////////////
 
-	///////////////////////////////////////////////////////////////////////////////
-	// -----------------------------	AJAX 	  ------------------------------ //
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    // -----------------------------    AJAX       ------------------------------ //
+    ///////////////////////////////////////////////////////////////////////////////
 
-	///////////////////////////////////////////////////////////////////////////////
-	// -------------------------	UTILITIES 	  ------------------------------ //
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    // -------------------------    UTILITIES       ------------------------------ //
+    ///////////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Return the decoded output
-	 * @return string
-	 */
-	public function decodedOutput()
-	{
-		if ($this->value)
-			return ci()->encrypt->decode($this->value);
-		else
-			return null;
-	}
+    /**
+     * Return the decoded output
+     * @return string
+     */
+    public function decodedOutput()
+    {
+        if ($this->value)
+            return ci()->encrypt->decode($this->value);
+        else
+            return null;
+    }
 }
